da1cfb3a93ee3e997fafe1113f46211b41d1bea3
Jonathan Fox <j.fox@outlook.com>
2021-09-26 12:43:30 -0300

    Add Icon and fixes to Auth Screen

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { NativeBaseProvider, Center, Button, Heading, Text} from "native-base"
+import { NativeBaseProvider, Center, Button, Heading, Text, Icon, HStack} from "native-base"
+import { Ionicons } from "@expo/vector-icons";
 import * as Google from "expo-google-app-auth"
 
 
@@ -29,16 +30,21 @@ export default function Auth({navigation}) {
                 if(userResult && tokenResult) navigation.navigate('Home')
             }
         } catch (error) {
-            console.log(error)
+            console.log(error.message)
         }
     }
 
     return (
         <NativeBaseProvider>
-            <Center flex={1} flexDirection='column' safeArea>
-                <Heading size='xl' m={-2}>Bienvenido!</Heading>
-                <Text>Iniciar sesión para continuar</Text>
-                <Button size='md' m={5} bg='indigo.500' paddingX={10} onPress={handleLoginPress}>Ingresar</Button>
+            <Center flex={1} flexDirection='column' bg='white' safeArea>
+                <Heading size='xl' m={-2}>Welcome!</Heading>
+                <Text>Login to continue</Text>
+                <Button size='md' m={5} bg='indigo.500' paddingX={10} onPress={handleLoginPress}>
+                    <HStack space={2}>
+                        <Icon as={Ionicons} name='logo-google' size={6} color='lightText' />
+                        <Text color='lightText'>Login</Text>
+                    </HStack>
+                </Button>
             </Center>
         </NativeBaseProvider>
     )
